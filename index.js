482d625d4e10fbc635b70e8973e93b0092eed79d
Sayan Dutta <rockistuart@gmail.com>
2023-08-03 05:27:06 +0530

    Action type, payload and Action creators

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 import { legacy_createStore as createStore } from "redux";
 
+// ACTION TYPES
+const INCREMENT_BY_1 = "INCREMENT_BY_1";
+const DECREMENT_BY_1 = "DECREMENT_BY_1";
+const INCREMENT_BY_VAL = "INCREMENT_BY_VAL";
+const DECREMENT_BY_VAL = "DECREMENT_BY_VAL";
+
 const reducer = (state = { count: 0 }, action) => {
-  switch (action.type) {
-    case "INCREMENT":
-      return { count: state.count + 1 };
-    case "DECREMENT":
-      return { count: state.count - 1 };
-    default:
-      return state;
-  }
-}
+	switch (action.type) {
+		case INCREMENT_BY_1:
+			return { count: state.count + 1 };
+		case DECREMENT_BY_1:
+			return { count: state.count - 1 };
+		case INCREMENT_BY_VAL:
+			return { count: state.count + action.payload };
+		case DECREMENT_BY_VAL:
+			return { count: state.count - action.payload };
+		default:
+			return state;
+	}
+};
+
+// ACTION CREATORS
+const incrementBy1Action = () => {
+	type: INCREMENT_BY_1;
+};
+const incrementByValAction = (payload) => {
+	type: INCREMENT_BY_VAL, payload;
+};
+const decrementBy1Action = () => {
+	type: DECREMENT_BY_1;
+};
+const decrementByValAction = (payload) => {
+	type: DECREMENT_BY_VAL, payload;
+};
 
 const store = createStore(reducer);
-console.log(store.getState()); // getState() returns the current state of the store
