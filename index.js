242d1fd106b3bcb142d2527ce8d2259ce8e292b3
Sayan Dutta <rockistuart@gmail.com>
2023-08-03 05:20:27 +0530

    getState()

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import { legacy_createStore as createStore } from "redux";
 
-// reducer is a pure function that takes in the current state and an action and returns the next state
 const reducer = (state = { count: 0 }, action) => {
   switch (action.type) {
     case "INCREMENT":
@@ -11,16 +10,6 @@ const reducer = (state = { count: 0 }, action) => {
       return state;
   }
 }
-// reducer : https://speakerdeck.com/stevekinney/redux-workshop-2021-05-05?slide=3
 
 const store = createStore(reducer);
-console.log(store);
-/**
-  {
-    dispatch: [Function: dispatch],
-    subscribe: [Function: subscribe],
-    getState: [Function: getState],
-    replaceReducer: [Function: replaceReducer], -> replaces one reducer with other: used for code splitting
-    '@@observable': [Function: observable]
-  }
- */
+console.log(store.getState()); // getState() returns the current state of the store
