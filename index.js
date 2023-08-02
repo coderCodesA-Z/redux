94bfdb0321af76a8c5ad964cb2d10dc8e3bd8f3a
Sayan Dutta <rockistuart@gmail.com>
2023-08-03 05:17:59 +0530

    Reducer and Store

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-import { compose, bindActionCreators, createStore, applyMiddleware  } from "redux";
+import { legacy_createStore as createStore } from "redux";
 
-const makeLouder = (str) => str.toUpperCase();
-const repeatThreeTimes = (str) => str.repeat(3);
-const embolden = (str) => str.bold();
+// reducer is a pure function that takes in the current state and an action and returns the next state
+const reducer = (state = { count: 0 }, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return { count: state.count + 1 };
+    case "DECREMENT":
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
+}
+// reducer : https://speakerdeck.com/stevekinney/redux-workshop-2021-05-05?slide=3
 
-// const makeLouderRepeatThreeTimesAndEmbolden = (str) =>
-// 	embolden(repeatThreeTimes(makeLouder(str)));
-
-const makeLouderRepeatThreeTimesAndEmbolden = compose(
-	embolden,
-	repeatThreeTimes,
-	makeLouder,
-);
-
-console.log(makeLouderRepeatThreeTimesAndEmbolden("Hello"));
-
-// compose()
+const store = createStore(reducer);
+console.log(store);
+/**
+  {
+    dispatch: [Function: dispatch],
+    subscribe: [Function: subscribe],
+    getState: [Function: getState],
+    replaceReducer: [Function: replaceReducer], -> replaces one reducer with other: used for code splitting
+    '@@observable': [Function: observable]
+  }
+ */
