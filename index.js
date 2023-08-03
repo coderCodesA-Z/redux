15e42008904566934ea69be3a4be87376bae8dd2
Sayan Dutta <rockistuart@gmail.com>
2023-08-03 12:34:16 +0530

    A Big Complex Reducer

--- index.js
+++ index.js
@@ -1,57 +1,38 @@
-import {
-	bindActionCreators,
-	compose,
-	legacy_createStore as createStore,
-} from "redux";
+import { legacy_createStore as createStore } from "redux";
 
-const INCREMENT_BY_1 = "INCREMENT_BY_1";
-const DECREMENT_BY_1 = "DECREMENT_BY_1";
-const INCREMENT_BY_VAL = "INCREMENT_BY_VAL";
-const DECREMENT_BY_VAL = "DECREMENT_BY_VAL";
+const initialState = {
+	users: [
+		{ id: 1, name: "Mark" },
+		{ id: 2, name: "Rob" },
+	],
+	tasks: [
+		{
+			title: "Buy More Energy Drinks",
+		},
+		{
+			title: "Cycle for 45min",
+		},
+	],
+};
 
-const reducer = (state = { count: 0 }, action) => {
+// if the state gets complex and we keep on adding to the same reducer, the reducer will start to become messy
+// TIME TO REFACTOR
+const reducer = (state = initialState, action) => {
 	switch (action.type) {
-		case INCREMENT_BY_1:
-			return { count: state.count + 1 }; // state is not mutated, return new state every time
-		case DECREMENT_BY_1:
-			return { count: state.count - 1 };
-		case INCREMENT_BY_VAL:
-			return { count: state.count + action.payload };
-		case DECREMENT_BY_VAL:
-			return { count: state.count - action.payload };
+		case "ADD_USER":
+			return {
+				...state,
+				users: [...state.users, action.payload],
+			};
+		case "ADD_TASK":
+			return {
+				...state,
+				tasks: [...state.tasks, action.payload],
+			};
 		default:
 			return state;
 	}
-};
-
-const incrementBy1Action = () => ({ type: INCREMENT_BY_1 });
-const incrementByValAction = (payload) => ({ type: INCREMENT_BY_VAL, payload });
-const decrementBy1Action = () => ({ type: DECREMENT_BY_1 });
-const decrementByValAction = (payload) => ({ type: DECREMENT_BY_VAL, payload });
+}
 
 const store = createStore(reducer);
 // multiple stores is an anti pattern. There should be one store which acts as provider
-
-const bindActionsManual = (fnsObj, dispatcher) => {
-	const boundActionsObj = {};
-	for (const fnObj in fnsObj) {
-		boundActionsObj[fnObj] = compose(dispatcher, fnsObj[fnObj]);
-	}
-	return boundActionsObj;
-};
-const actionsMan = bindActionsManual(
-	{ incrementBy1Action, incrementByValAction },
-	store.dispatch
-);
-console.log(store.getState());
-actionsMan.incrementBy1Action();
-console.log(store.getState());
-
-const action = bindActionCreators(
-	{ incrementBy1Action, incrementByValAction },
-	store.dispatch
-);
-
-console.log(store.getState());
-action.incrementBy1Action();
-console.log(store.getState());
